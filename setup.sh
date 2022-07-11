136dc1f682651c49932a6a6e6a3cc0d43259bc04
Ashley Thompson <workashleythompson@gmail.com>
2022-07-11 16:00:53 +1000

    ammends basic folder structure for iev application & sequelize

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@ which psql > /dev/null 2>&1
 EXITCODE=$?
 
 if [[ $EXITCODE -eq 0 ]]; then
-  echo "Creating Postgres Admin user..."
+  echo "---------------------------------"
+  echo ">> Creating Postgres Admin user..."
+  echo "In order for the API to be configured correctly use default password 'password'"
+  echo "when creating a new Postgres user; or change the config credentials in ./config/config.json"
+  echo "---------------------------------"
   # Create the Postgres super user for DB config
   sudo -u postgres createuser -s admin -P
   exit
