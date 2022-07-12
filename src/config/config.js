ad3431e4e2c22d5861a0d0bf700feab1de340110
Ashley Thompson <workashleythompson@gmail.com>
2022-07-12 16:49:32 +1000

    adds .sequelizerc file to configure use of sequelize with .env variables

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'iev_development',
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'iev_test',
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'DATABASE_URL',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
