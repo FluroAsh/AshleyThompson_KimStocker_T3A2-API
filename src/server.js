cb91f309155b732b743a49cf1717771d481d9d93
Ashley Thompson <workashleythompson@gmail.com>
2022-07-12 18:04:18 +1000

    adds jest testing suite, dependencies & tests folder to src

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// TODO: Add CORS once localhost version stable
+
+// TEMP: Remove when we add Express Routers for various models
+// EG >> app.use('/', router) for charging stations
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
 
 module.exports = {
   app,
---
+++ src/tests/example.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const { app } = require('../server.js');
+// GUIDE: https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/
+// JEST MATCHERS: https://jestjs.io/docs/using-matchers
+
+describe('Test the root path', () => {
+  test('It should respond to the GET method', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+});
