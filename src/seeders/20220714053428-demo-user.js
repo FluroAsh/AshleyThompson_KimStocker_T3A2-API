16147460428ded7affc151c180c14ae6e9131c03
Kim Stocker <kimbstocker@gmail.com>
2022-07-14 16:03:40 +1000

    amend .sequelizerc file and create User model

---
+++ src/seeders/20220714053428-demo-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    return queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      userName: 'Test1',
+      email: 'example@example.com',
+      password: '123456',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    return queryInterface.bulkDelete('Users', null, {});
+
+  }
+};
+
+
