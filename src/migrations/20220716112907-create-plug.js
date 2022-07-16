6510f8cc327105ebb391843eac1cbecc63ad577f
Ashley Thompson <workashleythompson@gmail.com>
2022-07-16 22:18:24 +1000

    removed extra migration for constraints, just moved into the original creater-user migration

--- src/migrations/20220716112907-create-plug.js
+++ src/migrations/20220716112907-create-plug.js
@@ -1,30 +1,16 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /** TODO: Add validation for columns in new migration */
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Plugs', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      firstName: {
-        type: Sequelize.STRING,
-      },
-      lastName: {
-        type: Sequelize.STRING,
-      },
-      email: {
+      plugName: {
         type: Sequelize.STRING,
         unique: true,
-      },
-      username: {
-        type: Sequelize.STRING,
-        unique: true,
-      },
-      password: {
-        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
@@ -37,6 +23,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Plugs');
   },
 };
