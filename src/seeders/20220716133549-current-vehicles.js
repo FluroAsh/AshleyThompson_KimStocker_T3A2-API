518e359c6a5a4936d3a61b61a1cc4cc1d922fc58
Ashley Thompson <workashleythompson@gmail.com>
2022-07-18 13:15:27 +1000

    adds additional car to current-vehicles seeder

--- src/seeders/20220716133549-current-vehicles.js
+++ src/seeders/20220716133549-current-vehicles.js
@@ -5,7 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert('Vehicles', [
       {
         make: 'Audi',
-        model: 'e-tron suv',
+        model: 'e-tron SUV',
         variant: 'e-tron SUV 50',
         PlugId: 2,
         createdAt: new Date(),
@@ -13,9 +13,17 @@ module.exports = {
       },
       {
         make: 'Audi',
-        model: 'e-tron suv',
+        model: 'e-tron SUV',
         variant: 'e-tron SUV 55',
         PlugId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        make: 'Audi',
+        model: 'e-tron Sportback',
+        variant: 'e-tron SUV 55',
+        PlugId: 3,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
