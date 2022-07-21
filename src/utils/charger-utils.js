dfc3b4f099e951357bbce21ae2bdc217072bb337
Kim Stocker <kimbstocker@gmail.com>
2022-07-21 14:07:37 +1000

    add eager load User and Address for charger

--- src/utils/charger-utils.js
+++ src/utils/charger-utils.js
@@ -1,12 +1,22 @@
+const { PublicAccessBlockConfiguration } = require("@aws-sdk/client-s3");
 const db = require("../models");
-const { Charger, User, Address } = db;
+const { Charger, User, Address, Plug } = db;
 const Op = db.Sequelize.Op;
 
 /** Returns a promise */
 async function getAllChargers() {
   try {
     const chargers = await Charger.findAll({
-      include: Address,
+      include: [
+        {
+          model: Address,
+          as: "Address",
+        },
+        {
+          model: User,
+          as: "User",
+        }
+      ],
     });
 
     return chargers;
@@ -17,13 +27,23 @@ async function getAllChargers() {
 }
 
 async function getOneCharger(id) {
+  // TODO: eager loading doesnt work
   try {
-    const charger = await Charger.findOne(
-      { where: { id: `${id}` } },
-      {
-        include: User,
-      }
-    );
+    const charger = await Charger.findOne({
+      where: { id },
+
+      include: [
+        {
+          model: Address,
+          as: "Address",
+        },
+        {
+          model: User,
+          as: "User",
+        },
+        
+      ],
+    });
 
     return charger;
   } catch (err) {
