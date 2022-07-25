ba1b1695969c67e4ef147d51f6e48778c1a0c2a4
Kim Stocker <kimbstocker@gmail.com>
2022-07-25 18:28:44 +1000

    add casual fake data package and amend User seed file

--- src/seeders/20220714053428-demo-user.js
+++ src/seeders/20220714053428-demo-user.js
@@ -1,33 +1,21 @@
 "use strict";
 
+const casual = require("casual");
+
+const users = [...Array(10)].map((user) => ({
+  firstName: casual.first_name,
+  lastName: casual.last_name,
+  email: casual.username,
+  username: casual.email,
+  password: casual.password,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}));
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     console.log("adding seed Users");
-    return queryInterface.bulkInsert("Users", [
-      {
-        firstName: "John",
-        lastName: "Doe",
-        username: "Test1",
-        email: "example@example.com",
-        // Generated using: https://bcrypt-generator.com/ (10 rounds)
-        // Decrypted: 123456
-        password:
-          "$2a$10$SV9DvTevkzYqqT/cpPDo4uPiRMyYvyB4sTigt9S4sIRa7cxiQSyzy",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        firstName: "Ashley",
-        lastName: "Thompson",
-        username: "Ash",
-        email: "ash@test.com",
-        // Decrypted: abc123
-        password:
-          "$2a$10$mKNvCDgNoas4LWYmuA3Q1.6u6csAkpxV1e4nrr4cUTIvjYjVTbk0K",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    return queryInterface.bulkInsert("Users", users);
   },
 
   async down(queryInterface, Sequelize) {
