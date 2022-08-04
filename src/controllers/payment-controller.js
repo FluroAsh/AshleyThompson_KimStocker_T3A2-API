9ce170c1086305c4feff3eb67944726eb82b1c85
Kim Stocker <kimbstocker@gmail.com>
2022-08-05 01:24:53 +0200

    delete test stripe key

--- src/controllers/payment-controller.js
+++ src/controllers/payment-controller.js
@@ -43,6 +43,7 @@ async function createCheckoutSession(req, res) {
       mode: "payment",
 
       //TODO: set success and canceled URLs
+      //TODO: use receipt url
       success_url: `http://localhost:3000`,
       cancel_url: `http://localhost:3000`,
     });
@@ -56,43 +57,34 @@ async function createCheckoutSession(req, res) {
 }
 
 // const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-
-const testEndpointSecret =
-  "whsec_024a4088719c941ebb0e7c5a90b6739c0b2dc22fbb19f7b4409d12c83830309b";
+// add test secret from stripe cli for testing
+const testEndpointSecret = undefined;
 const endpointSecret = testEndpointSecret || process.env.STRIPE_WEBHOOK_SECRET;
 
 async function webHook(req, res) {
-
   const sig = req.headers["stripe-signature"];
 
   let event;
-  let receipt_url
+  let receipt_url;
 
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-
   } catch (err) {
     res.status(400).send(`Error: ${err.message}`);
     return;
   }
 
-  console.log("EVENT PASSED", event);
-  // Handle the event
+  // TODO: Handle other events
   switch (event.type) {
     case "payment_intent.succeeded":
-
       const metadata = event.data.object.metadata;
       receipt_url = event.data.object.receipt_url;
-  
+
       const { userId, bookingId } = metadata;
-  
+
       const booking = await getBookingById(bookingId);
       booking.status = "paid";
       booking.save();
-
-      console.log("THIS IS NEW BOOKING STATUS SAVED", booking)
-
-      //TODO: Handle sucess event such as update booking status, unavailability table etc.
       // Then define and call a function to handle the event payment_intent.succeeded
       break;
     // ... handle other event types
